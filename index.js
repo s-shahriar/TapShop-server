1e96dd02235c660b3d89a9e7183fb63d18c89d44
s-shahriar <ksnkkc@gmail.com>
2024-08-16 11:10:44 +0600

    mongoDB init

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ app.use(cookieParser());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wpwwlgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
